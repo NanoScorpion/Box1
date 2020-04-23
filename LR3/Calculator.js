c40fd5625e8569f35cf916758b83d694e95c3370
NanoScorpion <63855589+NanoScorpion@users.noreply.github.com>
2020-04-23 20:12:44 +0300

    Create Calculator.js

---
+++ LR3/Calculator.js
@@ -0,0 +1,24 @@
+﻿var a;
+var b;
+var c;
+var isSunk = false;
+while (isSunk == false)
+{
+	a = prompt("Введите первое число:");
+	b = prompt("Введите второе число:");
+	c = prompt("Введите третье число:");
+	if (a > b && a > c)
+	{
+		alert("Первое число самое большое");
+	} else {
+	if (b > a && b > c)
+	{
+		alert("Второе число самое большое");
+	} else {
+	if (c > a && c > b)
+	{
+		alert("Третье число самое большое");
+	}
+	}
+	}	
+}
