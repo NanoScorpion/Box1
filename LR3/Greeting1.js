b20886f6acda494033bb6b757f77c0a77fab4f67
NanoScorpion <63855589+NanoScorpion@users.noreply.github.com>
2020-04-23 20:13:19 +0300

    Create Greeting1.js

---
+++ LR3/Greeting1.js
@@ -0,0 +1,14 @@
+﻿var a;
+var isSunk = false;
+while (isSunk == false)
+{
+a = prompt("Введите своё имя:");
+if (a == 'John')
+{
+alert("Добро пожаловать John!");
+}
+else
+{
+alert("Я вас не зню!");
+}
+}
